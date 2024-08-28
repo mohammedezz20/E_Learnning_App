3a14cc9e0cc3161f53675c97a69eba758af056dd
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-08-29 01:01:53 +0300

    add auth state

--- lib/features/Auth/presentation/cubit/auth_state.dart
+++ lib/features/Auth/presentation/cubit/auth_state.dart
@@ -1,10 +1,18 @@
 part of 'auth_cubit.dart';
 
-abstract class AuthState extends Equatable {
+abstract class AuthState {
   const AuthState();
-
-  @override
-  List<Object> get props => [];
 }
 
 class AuthInitial extends AuthState {}
+class AuthChangePassVisibilityState extends AuthState{}
+class AuthRememberUserState extends AuthState{}
+
+
+class AuthLoginLoadingState extends AuthState{}
+class AuthLoginErrorState extends AuthState{
+  final String errorMessage;
+  AuthLoginErrorState({required this.errorMessage});
+}
+class AuthLoginSuccessState extends AuthState{}
+
