efddc2807777118dac048c27e47aa54add7a083e
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-15 20:28:35 +0300

    refactor fetch courses method to cubit

--- lib/features/Cources/presentation/cubit/cources_state.dart
+++ lib/features/Cources/presentation/cubit/cources_state.dart
@@ -1,6 +1,6 @@
 part of 'cources_cubit.dart';
 
-abstract class CourcesState extends Equatable {
+abstract class CourcesState  {
   const CourcesState();
 
   @override
@@ -8,3 +8,18 @@ abstract class CourcesState extends Equatable {
 }
 
 class CourcesInitial extends CourcesState {}
+
+class ChangeIsSavedState extends CourcesState {
+  final bool isSaved;
+  ChangeIsSavedState({required this.isSaved});
+}
+
+class GetAllCoursesLoadingState extends CourcesState {}
+class GetAllCoursesSuccessState extends CourcesState {
+ final List<CourseEntity> courses;
+  GetAllCoursesSuccessState({required this.courses});
+}
+class GetAllCoursesErrorState extends CourcesState {
+  final String error;
+  GetAllCoursesErrorState({required this.error});
+}
