bc7b5ce3b026e2250e2f29b365c7bd879b04d0b0
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-15 20:27:59 +0300

    refactor courses screen UI

--- lib/features/Cources/presentation/pages/all_courses_screen.dart
+++ lib/features/Cources/presentation/pages/all_courses_screen.dart
@@ -1,5 +1,8 @@
 import 'package:e_learning_app/config/themes/colors.dart';
+import 'package:e_learning_app/core/Get%20it/setup_locator.dart';
+import 'package:e_learning_app/core/cach_helper.dart';
 import 'package:e_learning_app/features/Cources/data/all_courses_local_data.dart';
+import 'package:e_learning_app/features/Cources/domain/use_cases/all_courses_usecase.dart';
 import 'package:e_learning_app/features/Cources/presentation/cubit/cources_cubit.dart';
 import 'package:e_learning_app/features/Cources/presentation/widgets/all_courses_builder.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +13,22 @@ class AllCoursesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>CoursesCubit(),
+      create: (context)=>CoursesCubit(coursesUseCase:getIt.get<CourseUsecase>() ),
       child: BlocConsumer<CoursesCubit,CourcesState>(
         listener: (context,state){},
         builder: (context,state){
           return  SafeArea(
           child: Scaffold(
             appBar: AppBar(
-              backgroundColor: Colors.grey[200],
-              title: const Text('All Courses'),
+                backgroundColor: CachHelper.getData(key: 'isDark')
+             ?  const Color(0xff1f222a)
+             :   Colors.grey[200],
+            
+              title: Text('All Courses',style: TextStyle(
+                color:  CachHelper.getData(key: 'isDark')
+             ?  Colors.grey[200]
+            : const Color(0xff1f222a) 
+              ),),
               actions: <Widget>[
                 Padding(
                   padding: const EdgeInsets.only(right: 20.0),
