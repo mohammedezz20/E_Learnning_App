9aca3e74e7c58e47c7646bcdbe4f9d7323c730ae
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-08 07:21:28 +0300

    sign in domain layer

--- lib/features/Auth/domain/repositories/auth_repo.dart
+++ lib/features/Auth/domain/repositories/auth_repo.dart
@@ -1,14 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_learning_app/core/error_handling.dart';
+import 'package:e_learning_app/features/Auth/data/models/sign_in_model.dart';
 import 'package:e_learning_app/features/Auth/data/models/sign_up_model.dart';
 import 'package:e_learning_app/features/Auth/data/models/user_model.dart';
 import 'package:e_learning_app/features/Auth/domain/entities/user_entity.dart';
 
 abstract class IAuthRepo {
 
-  Future<Either<String,UserModel>>signIn({
-    required String email,
-    required String password,
+  Future<Either<Failure, UserDataEntity>> signIn({
+    required SignInModel signInModel
   });
   
   void saveUserData();
---
+++ lib/features/Auth/domain/usecases/sign_in_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_learning_app/core/error_handling.dart';
+import 'package:e_learning_app/core/use_cases.dart';
+import 'package:e_learning_app/features/Auth/data/models/sign_in_model.dart';
+import 'package:e_learning_app/features/Auth/domain/entities/user_entity.dart';
+import 'package:e_learning_app/features/Auth/domain/repositories/auth_repo.dart';
+
+class SignInUseCase extends UseCase<UserDataEntity,SignInModel>{
+   SignInUseCase({required this.authRepo});
+   final IAuthRepo authRepo;
+
+  @override
+  Future<Either<Failure, UserDataEntity>> call([SignInModel? param]) {
+  return authRepo.signIn(signInModel: param!);
+  }
+}
