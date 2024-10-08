4b3521dbb4c441dfad7b6c7788cd6d7d9b4988a7
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-08 06:31:29 +0300

    add reset pass screen UI

---
+++ lib/features/Auth/presentation/pages/ResetPassword/forget_password_screen.dart
@@ -0,0 +1,101 @@
+import 'dart:ui';
+import 'package:e_learning_app/config/themes/colors.dart';
+import 'package:e_learning_app/core/cach_helper.dart';
+import 'package:e_learning_app/core/utils/widgets/CustomFormField.dart';
+import 'package:e_learning_app/core/utils/widgets/custom_button.dart';
+import 'package:e_learning_app/features/Auth/presentation/cubit/auth_cubit.dart';
+import 'package:e_learning_app/features/Auth/presentation/pages/ResetPassword/verify_code_screen.dart';
+import 'package:e_learning_app/features/Auth/presentation/widgets/reset%20pass%20widgets/app_bar.dart';
+import 'package:e_learning_app/features/Auth/presentation/widgets/reset_pass_image.dart';
+import 'package:e_learning_app/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ForgetPasswordScreen extends StatelessWidget {
+  const ForgetPasswordScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+  var phoneController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+     return BlocConsumer<AuthCubit,AuthState>(
+      listener: (context,state){
+      },
+      builder: (context1,state){
+            var watchCubit=context1.watch<AuthCubit>();
+      return Scaffold(
+          body: SafeArea(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                       const ResetPasswordAppBar(),
+                       
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const ResetPassImage(image: 'assets/forgetpassword.png',),
+                          Text(
+                              'Enter your phone number',textAlign: TextAlign.center,
+                              style: Theme.of(context1).textTheme.bodySmall?.copyWith(
+                                  fontSize: 22.sp
+                              )
+                          ),
+                          const SizedBox(
+                            height:35,
+                          ),
+                         Padding(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 8.0),
+                              child:CustomFormField(
+                                    sizedBoxHeight: 20,
+                                    controller:phoneController,
+                                    border: 9.0,
+                                    hintTextFontSize: 25.0.sp,
+                                    outLineBorderColor: AppColor.loginOptionBorder,
+                                    focusedBorderColor: AppColor.buttonColor,
+                                    backgroundColor: !CachHelper.getData(key: 'isDark')
+                                     ? const Color(0xfffafafa)
+                                     : const Color(0xff1f222a),
+                                    prefix:const Icon(Icons.phone),
+                                    hintText:  S.of(context1).password,
+                                    keyboardType: TextInputType.phone,
+                                    validator: (value) {
+                                     return watchCubit.passwordValidator(value);
+                                    },                      
+                                    )
+                       ),
+                             SizedBox(
+                            height:MediaQuery.of(context1).size.height/6,
+                          ),
+                          CustomButton(
+                            onPressed: (){
+                             Navigator.push(
+                                context1,
+                                MaterialPageRoute(
+                                  builder: (context){ 
+                                    return   BlocProvider.value(
+                                   value: context1.read<AuthCubit>(),
+                                   child:const VerifyCodeScreen()
+                                  );}
+                                ),
+                              );
+                            },
+                            text:'Send', backgroundColor: AppColor.blueColor, width: double.infinity,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+         
+        );
+     });
+     
+      }
+  }
+
---
+++ lib/features/Auth/presentation/pages/ResetPassword/new_password_screen.dart
@@ -0,0 +1,146 @@
+import 'dart:ui';
+import 'package:e_learning_app/config/themes/colors.dart';
+import 'package:e_learning_app/core/cach_helper.dart';
+import 'package:e_learning_app/core/utils/widgets/CustomFormField.dart';
+import 'package:e_learning_app/core/utils/widgets/custom_button.dart';
+import 'package:e_learning_app/features/Auth/presentation/cubit/auth_cubit.dart';
+import 'package:e_learning_app/features/Auth/presentation/widgets/reset%20pass%20widgets/app_bar.dart';
+import 'package:e_learning_app/features/Auth/presentation/widgets/reset_pass_image.dart';
+import 'package:e_learning_app/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class NewPasswordScreen extends StatelessWidget {
+   NewPasswordScreen({super.key});
+    final oldPassController = TextEditingController();
+    final newPassController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit,AuthState>(
+        listener: (context,state){
+          // if(state is Stu_Reset_Pass_SuccessState){
+          //   if(state.statusCode==200){
+          //   Navigator.push(
+          //       context,
+          //       MaterialPageRoute(
+          //         builder: (context) =>
+          //             SuccessfulResetPasswordScreen(),
+          //       ));
+          //   }
+          // }
+        },
+       builder: (context,state){
+          var watchCubit=context.watch<AuthCubit>();
+          var readCubit=context.read<AuthCubit>();
+          return Scaffold(
+            backgroundColor: AppColor.lightBackgroundColor,
+            body:  SafeArea(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            const ResetPasswordAppBar(),
+                            const SizedBox(height: 10,),
+                            const ResetPassImage(image:'assets/code.png',),
+                       
+                            const SizedBox(
+                              height: 25,
+                            ),
+                            Text(
+                                'Enter New Password',
+                                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                                    fontSize: 20
+                                )
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                    
+                         
+                                CustomFormField(
+                                  sizedBoxHeight: 20,
+                                  controller:oldPassController,
+                                  border: 9.0,
+                                  hintTextFontSize: 25.0.sp,
+                                  outLineBorderColor: AppColor.loginOptionBorder,
+                                  focusedBorderColor: AppColor.buttonColor,
+                                  backgroundColor: !CachHelper.getData(key: 'isDark')
+                                   ? const Color(0xfffafafa)
+                                   : const Color(0xff1f222a),
+                                   isPassword: watchCubit.isPasswordVisible,
+                                  prefix:const Icon(Icons.lock_rounded),
+                                  hintText:  S.of(context).password,
+                                  suffix: GestureDetector(
+                                  child:watchCubit.isPasswordVisible
+                                       ? const Icon(Icons.visibility_off)
+                                       : const Icon(Icons.visibility),
+                                       onTap: (){
+                                      readCubit.changePassVisibility();
+                                      },
+                                  ),
+                                  keyboardType: TextInputType.visiblePassword,
+                                  validator: (value) {
+                                   return watchCubit.passwordValidator(value);
+                                  },                      
+                                  ),
+
+                                  CustomFormField(
+                                  sizedBoxHeight: 20,
+                                  controller:newPassController,
+                                  border: 9.0,
+                                  hintTextFontSize: 25.0.sp,
+                                  outLineBorderColor: AppColor.loginOptionBorder,
+                                  focusedBorderColor: AppColor.buttonColor,
+                                  backgroundColor: !CachHelper.getData(key: 'isDark')
+                                   ? const Color(0xfffafafa)
+                                   : const Color(0xff1f222a),
+                                   isPassword: watchCubit.isPasswordVisible,
+                                  prefix:const Icon(Icons.lock_rounded),
+                                  hintText:  S.of(context).password,
+                                  suffix: GestureDetector(
+                                  child:watchCubit.isPasswordVisible
+                                       ? const Icon(Icons.visibility_off)
+                                       : const Icon(Icons.visibility),
+                                       onTap: (){
+                                      readCubit.changePassVisibility();
+                                      },
+                                  ),
+                                  keyboardType: TextInputType.visiblePassword,
+                                  validator: (value) {
+                                   return watchCubit.passwordValidator(value);
+                                  },                      
+                                  ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                           CustomButton(
+                                onPressed: (){
+                                  // if(formkey6.currentState!.validate()) {
+                                  //   A.resetPass(
+                                  //     context: context
+                                  //       ,
+                                  //       oldPass: oldPasscontroller.text,
+                                  //       newPass: newPasscontroller.text);
+                                  // }
+                                },
+                                text:'Submit', backgroundColor: AppColor.blueColor, width: double.infinity,
+                              ),
+                             
+                    
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+          );
+       },
+    );
+  }
+}
